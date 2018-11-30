45f260c0df65ea4aa04506a1d9c6e0379c1d7530
lbf910924 <596572348@qq.com>
2018-11-30 15:32:14 +0800

    分享

--- src/routes/Other/Share.js
+++ src/routes/Other/Share.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'dva';
-
+import styles from './Share.less';
 @connect(state => ({
   global: state.global,
 }))
@@ -8,8 +8,15 @@ export default class Share extends React.Component {
 
   render(){
     return(
-      <div>
-        分享详情
+      <div className={styles.share}>
+      <img src={require('../../assets/share/bask_bg_01.jpg')}  className={styles.shareimg}/>  
+      <div className={styles.sharemiddle}>
+        <div className={styles.shareicon}><img src={require('../../assets/share/bask_goldcoin.png')} /><span>张**</span></div>
+        <div className={styles.sharetip}>2018年回报</div>
+        <div className={styles.sharemoney}><img src={require('../../assets/share/bask_goldcoin.png')} />204,580.32</div>
+        <p className={styles.shareword}>超越了23.12%的全国投友</p>
+        </div>
+        <div className={styles.sharebottom}><img src={require('../../assets/share/bask_bg_03.jpg')}  className={styles.shareimg}/> </div>
       </div>
     )
   }
