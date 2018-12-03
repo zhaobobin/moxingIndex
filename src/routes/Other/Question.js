3c398ee353f2a4280f40c3eed4171664773d5eed
lbf910924 <596572348@qq.com>
2018-12-03 17:00:01 +0800

    常见问题文案

--- src/routes/Other/Question.js
+++ src/routes/Other/Question.js
@@ -21,7 +21,7 @@ export default class Question extends React.Component {
   	const Panel = Collapse.Panel;
     return(
       <div className={styles.question}>
-        <Collapse  bordered={false} defaultActiveKey={['1']} accordion>
+        <Collapse  bordered={false}  defaultActiveKey={['1']} accordion>
           <Panel header="1、什么是智享服务？" key="1">
             <p>智享自动投标服务（本网站称“智享服务”）是基于去投网网贷大数据，运用自动投标、分散出借、风险管理等方法实现的帮助出借人快速出借、智能匹配债权的自动投标工具，同时兼顾出借人回报和流动性的需求而推出的智能出借服务。该服务的宗旨是帮助出借人尽快匹配到合适的散标债权，提高资金利用效率，让出借更便捷、轻松、智能。</p>
           </Panel>
