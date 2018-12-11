e984092dfbae3838ecd7d55ad17c31ad370d4d74
lbf910924 <596572348@qq.com>
2018-12-11 16:02:44 +0800

    分享接口调试，邀请好友加二维码，下载页面加二维码

--- src/routes/Other/Share.js
+++ src/routes/Other/Share.js
@@ -6,7 +6,42 @@ import ResultJson from '~/routes/Result/ResultJson'
   global: state.global,
 }))
 export default class Share extends React.Component {
+constructor(props) {
+    super(props);
+    this.loading = true;
+    this.state = {
+      data:{}
+    };
 
+  }
+componentDidMount(){
+    this.getshareData();
+ }
+
+  getshareData=()=>{
+  	let {userId, accessToken} = this.props.global.currentUser.userInfo;  
+  	console.log(this.props.global.currentUser.userInfo)
+    this.props.dispatch({
+      type: 'global/post',
+      url: '/api/personalCenterUser/getAssetDetails',
+      payload:{      	
+        userId,       
+        accessToken
+      },
+      
+      callback: (res) => {
+        this.loading = false;
+        if(res.code === 0){  
+        	console.log(res.data)
+          this.setState({
+          	
+          	data:res.data
+          	
+          })          
+        }
+      }
+    })
+  };
   redirect = (action) => {
     window.location.href = window.location.href + '&action=' + action;
     setTimeout(() => {
@@ -15,14 +50,15 @@ export default class Share extends React.Component {
   };
 
   render(){
+  	const {data} = this.state;
     return(
       <div className={styles.share}>
       <img src={require('../../assets/share/bask_bg_01.jpg')}  className={styles.shareimg}/>
       <div className={styles.sharemiddle}>
         <div className={styles.shareicon}><img src={require('../../assets/share/bask_bg_03.jpg')} /><span>张**</span></div>
-        <div className={styles.sharetip}>2018年回报</div>
-        <div className={styles.sharemoney}><img src={require('../../assets/share/bask_goldcoin.png')} />204,580.32元</div>
-        <p className={styles.shareword}>超越了23.12%的全国投友</p>
+        <div className={styles.sharetip}>{data.annualIncome}</div>
+        <div className={styles.sharemoney}><img src={require('../../assets/share/bask_goldcoin.png')} />{data.annualReturn}元</div>
+        <p className={styles.shareword}>超越了{data.percentage}%的全国投友</p>
         </div>
         <div className={styles.sharebottom}>
         <img src={require('../../assets/share/bask_bg_03.jpg')}  className={styles.shareimg} />
