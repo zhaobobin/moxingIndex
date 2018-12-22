1bbab29cfcf5631573a906eb62d8b56c628f99f6
lbf910924 <596572348@qq.com>
2018-12-22 15:50:04 +0800

    合规出借

--- src/routes/Found/About/AboutBusiness.js
+++ src/routes/Found/About/AboutBusiness.js
@@ -15,7 +15,7 @@ class AboutBusiness extends React.Component {
     return(
       <div className={styles.helpBusiness}>
         <p>“散标出借”是去投网平台推出的线上个人网络借贷撮合匹配及信息中介服务；</p><p>借款人通过去投网平台的审核后，方可在去投网平台上发布借款信息，出借人可通过去投网平台与借款人直接完成电子借款协议的签署，并通过银行存管系统进行款项的划转；</p><p>出借人通过等额本息的方式每月收取出借本金和利息回报。</p>
-        <img src={require('../../../assets/help/business.jpg')} />
+        <img src={require('../../../assets/help/business.png')} />
       </div>
     )
   }
