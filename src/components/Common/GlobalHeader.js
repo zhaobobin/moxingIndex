02e487c82312455ac8a488463eebb9c4d2a32786
zhaobobin <913849325@qq.com>
2019-02-27 16:38:43 +0800

    更新

--- src/components/Common/GlobalHeader.js
+++ src/components/Common/GlobalHeader.js
@@ -1,40 +1,21 @@
 import React from 'react';
-import { connect } from 'dva';
 import { Link } from 'dva/router'
-import { Icon } from 'antd'
-import { getTitle } from '~/utils/utils'
 import styles from './GlobalHeader.less'
 
-import logo from '~/assets/com/logo.png'
+import logo from '~/assets/com/m_nav_logo@2x.png'
+import account from '~/assets/com/m_myaccount@2x.png'
 
-@connect(state => ({
-  global: state.global,
-}))
-export default class GlobalHeader extends React.Component {
+export default function GlobalHeader () {
 
-  constructor(props){
-    super(props);
-    this.state = {
-      title: 'NavBar'
-    }
-  }
-
-  goBack = () => {
-    window.history.go(-1)
-  };
-
-  render(){
-
-    return(
-      <div className={styles.head}>
-        <Link to="/" className={styles.logo}>
-          <img src={logo} alt="logo"/>
-        </Link>
-        <Link to="/account" className={styles.account}>
-          <Icon type="user" />
-        </Link>
-      </div>
-    )
-  }
+  return(
+    <div className={styles.head}>
+      <Link to="/" className={styles.logo}>
+        <img src={logo} alt="logo"/>
+      </Link>
+      <Link to="/account" className={styles.account}>
+        <img src={account} alt="account"/>
+      </Link>
+    </div>
+  )
 
 }
